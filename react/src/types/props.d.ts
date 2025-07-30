d99821197b5af15683ab692fcf158ad2fd39e1b2
roing0328 <dev.hyoseung@gmail.com>
2025-07-31 00:08:33 +0900

    Feat : 타입 정의 하는 방법 - PrintValue.tsx <-> props.d.ts 로 타입 관계 형성

---
+++ react/src/types/props.d.ts
@@ -0,0 +1,9 @@
+/* 타입 키워드 방식 */
+type PrintValueProps = {
+  numberValue: number;
+  stringValue: string;
+  booleanValue: boolean;
+  arrayValue: number[];
+  objectValue: { name: string; age: number };
+  handleClick: () => void;
+};
