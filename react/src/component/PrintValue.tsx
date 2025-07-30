ee73c1c09507126e45921c8f28a020d81219cc75
roing0328 <dev.hyoseung@gmail.com>
2025-07-30 22:06:43 +0900

    Feat : 타입 쉽게 알아내기

--- react/src/component/PrintValue.tsx
+++ react/src/component/PrintValue.tsx
@@ -1,10 +1,13 @@
-/* 컴포넌트에 데이터 전달하기 2 (props) */
+/* 타입 쉽게 알아내기 */
 export default function PrintValue(props: {
   numberValue: number;
   stringValue: string;
-  booleanValue: true | false;
+  booleanValue: boolean;
   arrayValue: number[];
-  objectValue: { name: string; age: number };
+  objectValue: {
+    name: string;
+    age: number;
+  };
   handleClick: () => void;
 }) {
   return (
@@ -18,3 +21,46 @@ export default function PrintValue(props: {
     </>
   );
 }
+
+/* 비구조화 할당하기 */
+// export default function PrintValue(props: {
+//   numberValue: number;
+//   stringValue: string;
+//   booleanValue: true | false;
+//   arrayValue: number[];
+//   objectValue: { name: string; age: number };
+//   handleClick: () => void;
+// }) {
+//   const { numberValue, stringValue, booleanValue, arrayValue, objectValue, handleClick } = props;
+//   return (
+//     <>
+//       <p>number: {numberValue}</p>
+//       <p>string: {stringValue}</p>
+//       <p>boolean: {booleanValue.toString()}</p>
+//       <p>array: {arrayValue}</p>
+//       <p>object: {JSON.stringify(objectValue)}</p>
+//       <p>function: {handleClick.toString()}</p>
+//     </>
+//   );
+// }
+
+/* 컴포넌트에 데이터 전달하기 2 (props) */
+// export default function PrintValue(props: {
+//   numberValue: number;
+//   stringValue: string;
+//   booleanValue: true | false;
+//   arrayValue: number[];
+//   objectValue: { name: string; age: number };
+//   handleClick: () => void;
+// }) {
+//   return (
+//     <>
+//       <p>number: {props.numberValue}</p>
+//       <p>string: {props.stringValue}</p>
+//       <p>boolean: {props.booleanValue.toString()}</p>
+//       <p>array: {props.arrayValue}</p>
+//       <p>object: {JSON.stringify(props.objectValue)}</p>
+//       <p>function: {props.handleClick.toString()}</p>
+//     </>
+//   );
+// }
