f6c5470b71688fd11fd638293e10518a5496f978
rowing0328 <dev.hyoseung@gmail.com>
2025-08-04 10:26:51 +0900

    Feat : 미션 11. map 메서드를 활용한 리스트 렌더링 개요 - boolean 변수명 수정

--- react/src/component/FruitList.tsx
+++ react/src/component/FruitList.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default function FruitList() {
   const [items, setItems] = useState<string[]>(['사과', '바나나', '오렌지']);
   const handleFruitAdd = () => setItems((items) => [...items, '포도']);
-  const isIncluded = items.includes('포도');
+  const isGrapeAdded: boolean = items.includes('포도');
 
   return (
     <>
@@ -12,7 +12,7 @@ export default function FruitList() {
           <li key={index}>{item}</li>
         ))}
       </ul>
-      <button onClick={handleFruitAdd} disabled={isIncluded}>
+      <button onClick={handleFruitAdd} disabled={isGrapeAdded}>
         과일 추가
       </button>
     </>
